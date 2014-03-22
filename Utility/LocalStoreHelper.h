6b42320ebb12c585a0cd14a64cafcd822f1ad45e
allanxing <ft2130854@hotmail.com>
2014-03-23 00:29:47 +0800

    modify  workspacedate

---
+++ Utility/LocalStoreHelper.h
@@ -0,0 +1,13 @@
+//
+//  LocalStoreHelper.h
+//  MSBLogs_AllanXing
+//
+//  Created by allanxing on 3/21/14.
+//  Copyright (c) 2014 allan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalStoreHelper : NSObject
+
+@end
---
+++ Utility/LocalStoreHelper.m
@@ -0,0 +1,13 @@
+//
+//  LocalStoreHelper.m
+//  MSBLogs_AllanXing
+//
+//  Created by allanxing on 3/21/14.
+//  Copyright (c) 2014 allan. All rights reserved.
+//
+
+#import "LocalStoreHelper.h"
+
+@implementation LocalStoreHelper
+
+@end

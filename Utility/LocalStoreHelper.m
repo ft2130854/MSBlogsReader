443c66193cd7bb9f465a34a50c2fa859e0f760ba
allanxing <ft2130854@hotmail.com>
2014-03-25 17:01:06 +0800

    plist text

--- Utility/LocalStoreHelper.m
+++ Utility/LocalStoreHelper.m
@@ -10,4 +10,33 @@
 
 @implementation LocalStoreHelper
 
+- (id) init {
+    self = [super init];
+    if (self) {
+        NSString *errorDesc = nil;
+        NSPropertyListFormat format;
+        NSString *plistPath;
+        NSString *rootPath =
+        [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                             NSUserDomainMask, YES) objectAtIndex:0];
+        plistPath = [rootPath stringByAppendingPathComponent:@"Data.plist"];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:plistPath]) {
+            plistPath = [[NSBundle mainBundle] pathForResource:@"Data"
+                                                        ofType:@"plist"];
+        }
+        NSData *plistXML = [[NSFileManager defaultManager] contentsAtPath:plistPath];
+        NSDictionary *temp = (NSDictionary *)[NSPropertyListSerialization
+                                              propertyListFromData:plistXML
+                                              mutabilityOption:NSPropertyListMutableContainersAndLeaves
+                                              format:&format
+                                              errorDescription:&errorDesc];
+        if (!temp) {
+            NSLog(@"Error reading plist: %@, format: %d", errorDesc, format);
+        }
+//        self.personName = [temp objectForKey:@"Name"];
+//        self.phoneNumbers = [NSMutableArray arrayWithArray:[temp
+//                                                            objectForKey:@"Phones"]];
+    }
+    return self;
+}
 @end
